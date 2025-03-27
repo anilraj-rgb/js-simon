fe4fe4f6923e4a31d810ab191bdeb5dcf178fd35
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-27 16:32:12 +0100

    [UPD] list numbers

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Creo variabile countdown
+const countdown = document.getElementById('countdown'); 
+console.log(countdown);
+
+// Riprendo lista dei numeri randomici
+const numeriRandom = document.getElementById('numbers-list');
