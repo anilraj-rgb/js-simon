74e787b7f9eb492d7a66b0a0208157ec07df4660
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-27 16:33:38 +0100

    [UPD] contdown

--- js/script.js
+++ js/script.js
@@ -16,3 +16,19 @@ const form = document.getElementById('answers-form');
 
 //Numeri randomici fino a 50
 const random = Math.floor(Math.random() * 50) + 1;
+
+// Aggiorna il countdown ogni secondo
+let tempoRimasto = 5; 
+
+const timer = setInterval(() => {
+    if (tempoRimasto > 0) {
+        countdown.innerHTML = tempoRimasto; // Mostra il countdown sullo schermo
+        tempoRimasto--;
+    } else {
+        clearInterval(timer);
+        istructions.innerText = 'Inserisci i numeri che hai visto in precedenza'; // Mostra le istruzioni
+        form.classList.remove('d-none'); // Mostra il form
+        numeriRandom.classList.add('d-none'); // Nascondi la lista dei numeri   
+        countdown.innerHTML = 'Tempo scaduto'; // Mostra il messaggio di fine countdown
+    }
+}, 1000);
