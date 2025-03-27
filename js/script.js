94520c6b3467d436fa34ac6f82e1234a2acddd39
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-27 16:32:58 +0100

    [UPD] random number and botton

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ console.log(countdown);
 
 // Riprendo lista dei numeri randomici
 const numeriRandom = document.getElementById('numbers-list');
+
+//Riprendo le istruzioni
+const istructions = document.getElementById('instructions');
+
+//Riprendo bottone
+const bottone = document.querySelector('button');
+
+//Riprendo form
+const form = document.getElementById('answers-form');
+
+//Numeri randomici fino a 50
+const random = Math.floor(Math.random() * 50) + 1;
